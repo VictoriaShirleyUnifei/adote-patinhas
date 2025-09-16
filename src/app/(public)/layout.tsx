13f40cb7d8d2f33b5c482316e35f65514effee24
VictoriaShirleyUnifei <d2023002387@unifei.edu.br>
2025-09-15 23:26:58 -0300

    style: Responsividade do card transparente do PublicLayout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -24,30 +24,35 @@ export default function PublicLayout({
         />
 
         {/* Card transparente sobre a imagem */}
-        <Box
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-20 rounded-lg flex flex-col items-center text-center gap-7"
-          sx={{
-            backgroundColor: alpha(theme.palette.common.black, 0.5), // preto 50% transparente
-            width: "70%",
-            color: "white"
-          }}
-        >
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            color="white"
-            sx={{ textTransform: "uppercase" }}
+        <Box className="absolute inset-0 flex justify-center items-center">
+          <Box
+            className="w-full max-w-md sm:max-w-lg md:max-w-2xl p-6 sm:p-10 md:p-20 rounded-lg flex flex-col items-center text-center gap-4 sm:gap-7"
+            sx={{
+              backgroundColor: alpha(theme.palette.common.black, 0.5),
+              color: "white",
+            }}
           >
-            Adoção é um ato de amor!
-          </Typography>
-          <Typography
-            variant="h5"
-            fontWeight="bold"
-            color="white"
-            sx={{ textTransform: "uppercase" }}
-          >
-            Não compre, adote!
-          </Typography>
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              sx={{
+                textTransform: "uppercase",
+                fontSize: { xs: "1.5rem", sm: "2rem", md: "2.5rem" },
+              }}
+            >
+              Adoção é um ato de amor!
+            </Typography>
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              sx={{
+                textTransform: "uppercase",
+                fontSize: { xs: "1.2rem", sm: "1.5rem", md: "2rem" },
+              }}
+            >
+              Não compre, adote!
+            </Typography>
+          </Box>
         </Box>
       </div>
 
