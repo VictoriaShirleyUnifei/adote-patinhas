8462298bbc5aaca44bee6cb9b28aba95d1d4d532
VictoriaShirleyUnifei <d2023002387@unifei.edu.br>
2025-09-21 02:02:44 -0300

    fix: Remove importação não utilizada do componente Typography no Filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import React from "react";
 import CustomButton from "../Buttons/CustomButton";
