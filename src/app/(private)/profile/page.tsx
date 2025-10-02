164de4724c836d1da21939b41d6264d813d90ba1
VictoriaShirleyUnifei <d2023002387@unifei.edu.br>
2025-10-02 20:54:25 -0300

    feat: Adiciona página de perfil

---
+++ src/app/(private)/profile/page.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import CardAnimal from "@/components/Cards/CardAnimal";
+import Filter from "@/components/Filter/Filter";
+import ModalRegisterAnimal from "@/components/Modals/ModalRegisterAnimal";
+import { ArrowRight } from "@mui/icons-material";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import React from "react";
+
+export default function ProfilePage() {
+  const theme = useTheme();
+  const [openModal, setOpenModal] = React.useState(false);
+  const [viewProfile, setViewProfile] = React.useState(false);
+  const [viewRegisteredAnimals, setViewRegisteredAnimals] =
+    React.useState(false);
+  const [viewInterestList, setViewInterestList] = React.useState(false);
+
+  const handleViewProfile = () => {
+    setViewProfile(true);
+    setViewRegisteredAnimals(false);
+    setViewInterestList(false);
+  };
+
+  const handleViewRegisteredAnimals = () => {
+    setViewProfile(false);
+    setViewRegisteredAnimals(true);
+    setViewInterestList(false);
+  };
+
+  const handleViewInterestList = () => {
+    setViewProfile(false);
+    setViewRegisteredAnimals(false);
+    setViewInterestList(true);
+  };
+
+  const renderCards = (n: number) => {
+    return Array.from({ length: n }).map((_, index) => (
+      <CardAnimal key={index} />
+    ));
+  };
+
+  return (
+    <>
+      <div className="flex gap-4 min-h-screen flex-wrap">
+        <div className="flex flex-col gap-3 w-full md:w-1/4">
+          <Box
+            sx={{
+              backgroundColor: theme.palette.card.default,
+              color: theme.palette.secondary.main,
+              padding: "20px",
+              borderRadius: "8px",
+            }}
+          >
+            <Typography variant="h6" fontStyle={"semibold"}>
+              Perfil
+            </Typography>
+            <Stack spacing={1}>
+              <Button
+                variant="text"
+                onClick={handleViewProfile}
+                sx={{
+                  color: theme.palette.secondary.main,
+                  justifyContent: "flex-start",
+                  gap: 1,
+                  textTransform: "none",
+                }}
+                startIcon={<ArrowRight />}
+              >
+                <p className="font-bold">Dados pessoais</p>
+              </Button>
+              <Button
+                variant="text"
+                onClick={handleViewRegisteredAnimals}
+                sx={{
+                  color: theme.palette.secondary.main,
+                  justifyContent: "flex-start",
+                  gap: 1,
+                  textTransform: "none",
+                }}
+                startIcon={<ArrowRight />}
+              >
+                <p className="font-bold">Animais cadastrados</p>
+              </Button>
+              <Button
+                variant="text"
+                onClick={handleViewInterestList}
+                sx={{
+                  color: theme.palette.secondary.main,
+                  justifyContent: "flex-start",
+                  gap: 1,
+                  textTransform: "none",
+                }}
+                startIcon={<ArrowRight />}
+              >
+                <p className="font-bold">Lista de interesses</p>
+              </Button>
+            </Stack>
+          </Box>
+          <Filter />
+        </div>
+
+        {viewProfile && (
+          <div className="flex flex-col gap-3 flex-1">
+            
+          </div>
+        )}
+
+        {viewRegisteredAnimals && (
+          <div className="flex flex-col gap-3 flex-1">
+          <Box
+            sx={{
+              backgroundColor: theme.palette.card.default,
+              color: theme.palette.tertiary.main,
+              padding: "8px",
+              borderRadius: "8px",
+            }}
+          >
+            <p className="font-bold text-center px-6">
+              Animais cadastrados
+            </p>
+          </Box>
+          <div className="flex flex-wrap gap-3 flex-1 max-h-screen overflow-y-auto">
+            {renderCards(12)}
+          </div>
+        </div>
+        )}
+
+        {viewInterestList && (
+          <div className="flex flex-col gap-3 flex-1">
+          <Box
+            sx={{
+              backgroundColor: theme.palette.card.default,
+              color: theme.palette.tertiary.main,
+              padding: "8px",
+              borderRadius: "8px",
+            }}
+          >
+            <p className="font-bold text-center px-6">
+              Lista de interesses
+            </p>
+          </Box>
+          <div className="flex flex-wrap gap-3 flex-1 max-h-screen overflow-y-auto">
+            {renderCards(12)}
+          </div>
+        </div>
+        )}
+
+      </div>
+
+      <ModalRegisterAnimal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+      />
+    </>
+  );
+}
