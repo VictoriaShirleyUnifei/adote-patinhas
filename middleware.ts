1f88ee4021962c91642c0e026fb1b88e02afa4d3
VictoriaShirleyUnifei <d2023002387@unifei.edu.br>
2025-12-06 12:59:08 -0300

    feat: Atualiza middleware para redirecionar usuários com base no estado de autenticação

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,29 @@
-// middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/login', request.url))
+    console.log("Middleware rodando em:", request.nextUrl.pathname)
+
+  const token = request.cookies.get("session")?.value;
+  const { pathname } = request.nextUrl;
+
+  const publicRoutes = ["/login"];
+  const isPublic = publicRoutes.some(route => pathname.startsWith(route));
+
+  // Usuário DESLOGADO tentando acessar rota privada → 404 Not Found
+  if (!isPublic && !token) {
+  return NextResponse.redirect(new URL("/register", request.url));
+}
+
+
+  // Usuário LOGADO tentando acessar página pública → envia pro home
+  if (isPublic && token) {
+    return NextResponse.redirect(new URL("/home", request.url));
   }
-  
-  return NextResponse.next()
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/',
-}
+  matcher: ["/:path*"],
+};
